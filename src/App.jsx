5036d751c5df2049cc56efb2412164117eebc593
Marcel Wang <mrcel.wng@gmail.com>
2026-05-08 15:29:29 +0700

    First Initiate

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full h-screen flex flex-col">
+      <div className="w-full py-3 px-5 bg-pink-300">
+        <h1 className="text-2xl text-white font-bold italic">Qrees</h1>
+      </div>
+    </div>
+  );
+}
