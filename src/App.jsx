ee36aacdd80df5066bf843906f8db9cf72f58ccd
MrCel-7 <mrcel.wng@gmail.com>
2026-05-10 09:20:18 +0700

    Add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { FaBell, FaExclamation, FaEye, FaEyeSlash } from "react-icons/fa";
+import {
+  FaBell,
+  FaEye,
+  FaEyeSlash,
+  FaPiggyBank,
+  FaWallet,
+} from "react-icons/fa";
+import { FaArrowTrendDown, FaArrowTrendUp } from "react-icons/fa6";
 
 export default function App() {
   const username = "Marcel Wang";
@@ -34,8 +41,11 @@ export default function App() {
       </div>
 
       {/* Card */}
-      <div className="w-full bg-sky-400 p-5 mt-10 rounded-xl">
-        <h1 className="text-lg text-white text-shadow font-bold">Wallet</h1>
+      <div className="w-full bg-sky-400 p-5 mt-10 rounded-xl shadow-xl">
+        <div className="flex items-center  gap-3 text-lg text-white text-shadow font-bold">
+          <FaWallet />
+          <p>Wallet</p>
+        </div>
         <div className="my-10 flex justify-between">
           <div className="flex gap-4">
             <h1 className="text-white text-4xl font-semibold ">Rp</h1>
@@ -68,6 +78,24 @@ export default function App() {
             Savings: Rp {showWallet ? formatMoney(savings) : "*******"}
           </p>
         </div>
+      </div>
+
+      {/* Navigation */}
+      <div className="flex flex-col w-full gap-5">
+        <div className="grid grid-cols-2 mt-5 gap-5">
+          <button className="shadow-xl py-2 flex items-center justify-center gap-2 cursor-pointer text-white font-bold bg-green-400 rounded-xl">
+            <FaArrowTrendUp />
+            <p className="text-md">Income</p>
+          </button>
+          <button className="shadow-xl py-2 flex items-center justify-center gap-2 cursor-pointer text-white font-bold bg-red-400 rounded-xl">
+            <FaArrowTrendDown />
+            <p className="text-md">Expense</p>
+          </button>
+        </div>
+        <button className="shadow-xl py-2 flex items-center justify-center gap-2 cursor-pointer text-white font-bold bg-yellow-400 rounded-xl">
+          <FaPiggyBank />
+          <p className="text-md">Savings</p>
+        </button>
       </div>
     </div>
   );
