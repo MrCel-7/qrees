c198502c5cb15706a88633f0c940c730020fdffc
MrCel-7 <mrcel.wng@gmail.com>
2026-05-10 08:59:19 +0700

    Updating header

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,16 @@ export default function App() {
       {/* Header */}
       <div className="w-full py-5 flex justify-between items-center">
         <div className="flex gap-5 items-center">
-          <div className="p-3 bg-sky-200 w-fit rounded-full shadow-xl">
-            <img src="/user.png" className="w-8" />
+          <div className="p-2 bg-sky-200 w-fit rounded-full shadow-xl">
+            <img src="/user.png" className="w-5" />
           </div>
         </div>
 
-        <h1 className="text-2xl font-bold">QREES</h1>
+        <h1 className="text-lg font-bold">QREES</h1>
 
         <div className="flex items-center">
-          <div className="p-3 rounded-full bg-sky-200 shadow-xl">
-            <FaBell className="text-2xl text-blue-500 text-shadow" />
+          <div className="p-2 rounded-full bg-sky-200 shadow-xl">
+            <FaBell className="text-md text-blue-500 text-shadow" />
           </div>
         </div>
       </div>
@@ -36,7 +36,7 @@ export default function App() {
       {/* Card */}
       <div className="w-full bg-sky-400 p-5 mt-10 rounded-xl">
         <h1 className="text-lg text-white text-shadow font-bold">Wallet</h1>
-        <div className="my-13 flex justify-between">
+        <div className="my-10 flex justify-between">
           <div className="flex gap-4">
             <h1 className="text-white text-4xl font-semibold ">Rp</h1>
             <h1 className="font-semibold text-4xl text-white">
